61d1e9d7bab3684dcb5462006498338a4777b652
Sviatoslav <belmeha@gmail.com>
2017-07-11 23:58:36 +0300

    [ADD] Added .gitignore

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  stratum client
+//
+//  Created by landan on 7/11/17.
+//  Copyright © 2017 landsv. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
